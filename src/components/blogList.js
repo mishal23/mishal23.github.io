6feea5a919bb7c4a68eb2fe80eb6aef6832ead43
Mishal Shah <shahmishal1998@gmail.com>
2020-04-27 02:21:06 +0530

    index page listing, favicon icon added

---
+++ src/components/blogList.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+import Default from "../components/default"
+
+
+const BlogList = () => {
+  const data = useStaticQuery(
+  graphql`
+  query BlogList {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+            description
+          }
+        }
+      }
+    }
+  }
+`
+)
+  const { allMarkdownRemark } = data
+  
+  // const { currentPage, numPages } = pageContext
+
+  return(
+  <>
+  <Default></Default>
+    return(
+      <main role="main">
+    <div className="content">
+    {allMarkdownRemark.edges.map(({ node }) => (
+    <article class="main-article">
+      <div class="wrap-content">
+        <header class="header-article">
+          <h2 class="title-article"><a href="{}">{node.frontmatter.title}</a></h2>
+          <div class="post-date"><span>{node.frontmatter.date}&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
+        </header>
+        <p>{node.frontmatter.description} <Link to={node.fields.slug}>(Read more ...)</Link></p>
+      </div>
+    </article>
+  ))}
+    </div>
+    </main>
+    )
+  </>
+  )
+}
+
+export default BlogList
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,56 +5,19 @@ import React from "react"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 import Default from "../components/default"
+import BlogList from "../components/blogList"
 
 
-const IndexPage = ({ data, pageContext}) => {
-  console.log(data)
-  const { allMarkdownRemark } = data
-  const { currentPage, numPages } = pageContext
+const IndexPage = () => {
+  // const { allMarkdownRemark } = data
+  // const { currentPage, numPages } = pageContext
 
   return(
   <>
-  <Default></Default>
-  {allMarkdownRemark.edges.map(({ node }) => {
-    return(
-    <>
-    <article class="main-article">
-      <div class="wrap-content">
-        <header class="header-article">
-          <h2 class="title-article"><a href="{}">{}</a></h2>
-          <div class="post-date"><span>{}&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
-        </header>
-        <p>{} <a class="more" href="{}">(Read more...)</a></p>
-      </div>
-    </article>
-    </>
-    )
-  })}
+    <Default></Default>
+    <BlogList></BlogList>
   </>
   )
 }
 
 export default IndexPage
-
-
-export const query = graphql`
-  query blogPostsList($skip: Int!, $limit: Int!) {
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: $limit
-      skip: $skip
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            date
-          }
-        }
-      }
-    }
-  }
-`
