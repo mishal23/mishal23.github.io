7136a03cdbf7acf37fe38b584c5c432376184a2e
Mishal Shah <shahmishal1998@gmail.com>
2020-04-27 17:17:08 +0530

    remove wrong font link

--- src/components/default.js
+++ src/components/default.js
@@ -2,7 +2,7 @@ import React from "react"
 // import Header from "./header"
 // import PropTypes from "prop-types"
 import SideBar from "./sidebar"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import "./stylesheets/main.css"
 import "./stylesheets/mobile.css"
@@ -10,11 +10,8 @@ import "./stylesheets/mobile.css"
 
 const Default = () => {
 	return(
-		<>
-		<Link to="//fonts.googleapis.com/css?family=Finger+Paint|PT+Sans:400,700"></Link>
 		<SideBar>
 		</SideBar>
-		</>
     )
 }
 
