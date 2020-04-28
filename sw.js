aa673e40ffeba90a74690a2fe3cb38420bd1a592
mishal23 <shahmishal1998@gmail.com>
2020-04-28 20:49:22 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-34f3a54338438e193c31.js"
+    "url": "webpack-runtime-9f2c7461bee7a0bf3f61.js"
   },
   {
     "url": "framework-38299d20c7ca7a3581d4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5bd311d1cbb3b9a96757033cec29a67a"
+    "revision": "80574d53cfa84d8282d06358cf9954a1"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9c33eb7d4d15603ab76b.js"
